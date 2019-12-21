9b79ca205ca81440f1d84de94ceddd4003423125
homewin7 <2350235394@qq.com>
2019-12-21 23:48:32 +0800

    4

--- ServerSg90.c
+++ ServerSg90.c
@@ -1,6 +1,6 @@
 #include "ServerSg90.h"
 /**********************************************************************************************************
-*	函 数 名: void ServerSg90_Inter(SERVERSG90_BASE *base,uint16_t interupt_frequs)
+*	函 数 名: void ServerSg90_Inter(SERVERSG90_DEV *base,uint16_t interupt_frequs)
 *	功能说明: 该函数放入间隔调用的函数内(pin 模式下，pwm模式下不用)，调用周期最好是10us
 *	传    参: interupt_frequs调用间隔时间（单位us）
 *	返 回 值: 
@@ -13,7 +13,7 @@
 	  2.0ms-----------135度；  10.0%
 	  2.5ms-----------180度；  12.5% 
 *********************************************************************************************************/
-void ServerSg90_Inter(SERVERSG90_BASE *base,uint16_t interupt_frequs){
+void ServerSg90_Inter(SERVERSG90_DEV *base,uint16_t interupt_frequs){
 	base->count_us += interupt_frequs;
 	if(base->count_us >= 20000){
 		base->count_us = 0;
@@ -28,14 +28,14 @@ void ServerSg90_Inter(SERVERSG90_BASE *base,uint16_t interupt_frequs){
 	}
 }
 /**********************************************************************************************************
-*	函 数 名: void ServerSg90_Init(SERVERSG90_BASE *base,void(*serversg90_pwm_l)(void),void(*serversg90_pwm_h)(void))
+*	函 数 名: void ServerSg90_Init(SERVERSG90_DEV *base,void(*serversg90_pwm_l)(void),void(*serversg90_pwm_h)(void))
 *	功能说明: 控制接口初始化，确定控制接口，
 *	传    参: serversg90_pwm_l 将pwm管脚拉低的函数
 			  serversg90_pwm_h 将pwm管脚拉高的函数
 *	返 回 值: 
 *   说    明: 
 *********************************************************************************************************/
-void ServerSg90_Init(SERVERSG90_BASE *base,void(*serversg90_pwm_l)(void),void(*serversg90_pwm_h)(void)){
+void ServerSg90_Init(SERVERSG90_DEV *base,void(*serversg90_pwm_l)(void),void(*serversg90_pwm_h)(void)){
 	base->serversg90_pwm_h = serversg90_pwm_h;
 	base->serversg90_pwm_l = serversg90_pwm_l;
 
@@ -46,9 +46,7 @@ void ServerSg90_Init(SERVERSG90_BASE *base,void(*serversg90_pwm_l)(void),void(*s
 	base->pwm_mode	= PIN_MODE;
 }
 //增加直接使用pwm模式的方式
-void ServerSg90_InitPwmMode(SERVERSG90_BASE *base,void(*serversg90_pwm)(float pwm_pulse)){
-	base->serversg90_pwm_h = serversg90_pwm_h;
-	base->serversg90_pwm_l = serversg90_pwm_l;
+void ServerSg90_InitPwmMode(SERVERSG90_DEV *base,void(*serversg90_pwm)(float pwm_pulse)){
 
 	base->pwm_mode	= PWM_MODE;
 	base->serversg90_pwm = serversg90_pwm;
@@ -57,17 +55,17 @@ void ServerSg90_InitPwmMode(SERVERSG90_BASE *base,void(*serversg90_pwm)(float pw
 	ServerSg90Control(base,0);
 }
 /**********************************************************************************************************
-*	函 数 名: void ServerSg90Control(SERVERSG90_BASE *base,float angle){
+*	函 数 名: void ServerSg90Control(SERVERSG90_DEV *base,float angle){
 *	功能说明: 控制角度，angle的范围是0.00度-180.00度
 *	传    参: 
 *	返 回 值: 
 *   说    明:  控制的角度精度是可以达到180/1024度约0.18度
 *********************************************************************************************************/
-void ServerSg90Control(SERVERSG90_BASE *base,float angle){
+void ServerSg90Control(SERVERSG90_DEV *base,float angle){
 	if((angle >= 0.00) && (angle <= 180.00)){
 		base->angle = angle;	
 		if(base->pwm_mode == PWM_MODE){
-			base->serversg90_pwm(angle/18+2.5);
+			base->serversg90_pwm((angle/18+2.5)/100);
 		}else{
 			base->serversg90_h_timeus = 100.0/9.0*base->angle + 500.0;
 		}
--- ServerSg90.h
+++ ServerSg90.h
@@ -7,7 +7,7 @@ enum PWM_MODE
 	PIN_MODE = 0,
 	PWM_MODE = 1
 };
-typedef struct SERVERSG90_BASE
+typedef struct SERVERSG90_DEV
 {
 	void(*serversg90_pwm_l)(void);
 	void(*serversg90_pwm_h)(void);
@@ -19,10 +19,10 @@ typedef struct SERVERSG90_BASE
 
 	uint32_t period_count;
 	void(*serversg90_pwm)(float pwm_pulse);
-}SERVERSG90_BASE;
+}SERVERSG90_DEV;
 
-void ServerSg90_Inter(SERVERSG90_BASE *base,uint16_t interupt_frequs);
-void ServerSg90_Init(SERVERSG90_BASE *base,void(*serversg90_pwm_l)(void),void(*serversg90_pwm_h)(void));
-void ServerSg90_InitPwmMode(SERVERSG90_BASE *base,void(*serversg90_pwm)(float pwm_pulse));
-void ServerSg90Control(SERVERSG90_BASE *base,float angle);
+void ServerSg90_Inter(SERVERSG90_DEV *base,uint16_t interupt_frequs);
+void ServerSg90_Init(SERVERSG90_DEV *base,void(*serversg90_pwm_l)(void),void(*serversg90_pwm_h)(void));
+void ServerSg90_InitPwmMode(SERVERSG90_DEV *base,void(*serversg90_pwm)(float pwm_pulse));
+void ServerSg90Control(SERVERSG90_DEV *base,float angle);
 #endif
